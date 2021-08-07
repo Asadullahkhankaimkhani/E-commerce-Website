71a87669f78688ecbfe306eb5643858316c8c5ef
Asadullah Khan <41535758+Asadullahkhankaimkhani@users.noreply.github.com>
2021-08-08 01:25:14 +0500

    Register Form

---
+++ client/src/firebase.js
@@ -0,0 +1,12 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCtO74nGlTzgCAnES38yChDZSvbeHf3Pbc",
+  authDomain: "ecommerce-9490b.firebaseapp.com",
+  projectId: "ecommerce-9490b",
+  storageBucket: "ecommerce-9490b.appspot.com",
+  messagingSenderId: "923501273315",
+  appId: "1:923501273315:web:9a26c9b547a1861d3be5d6",
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
