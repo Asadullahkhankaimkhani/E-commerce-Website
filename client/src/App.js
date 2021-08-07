c563d367831094692a204f66ca4b58aa34b4175c
Asadullah Khan <41535758+Asadullahkhankaimkhani@users.noreply.github.com>
2021-08-07 20:00:44 +0500

    Ant Design  Nagivation

--- client/src/App.js
+++ client/src/App.js
@@ -2,14 +2,19 @@ import { Switch, Route } from "react-router-dom";
 import Login from "./pages/auth/Login";
 import Register from "./pages/auth/Register";
 import Home from "./pages/Home";
+import Header from "./components/nav/Header";
+import { Fragment } from "react";
 
 const App = () => {
   return (
-    <Switch>
-      <Route exact path="/" component={Home}></Route>
-      <Route exact path="/login" component={Login}></Route>
-      <Route exact path="/register" component={Register}></Route>
-    </Switch>
+    <Fragment>
+      <Header />
+      <Switch>
+        <Route exact path="/" component={Home}></Route>
+        <Route exact path="/login" component={Login}></Route>
+        <Route exact path="/register" component={Register}></Route>
+      </Switch>
+    </Fragment>
   );
 };
 
