f5fe8dbf37b742727d050962d12f7dd505153d04
Asadullah Khan <41535758+Asadullahkhankaimkhani@users.noreply.github.com>
2021-08-08 18:02:55 +0500

    Setup Redux

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,15 @@
+// React
 import { Switch, Route } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import { Fragment } from "react";
 
+// Pages
 import Login from "./pages/auth/Login";
 import Register from "./pages/auth/Register";
 import RegisterComplete from "./pages/auth/RegisterComplete";
 import Home from "./pages/Home";
 
+// Componnets
 import Header from "./components/nav/Header";
 
 import "react-toastify/dist/ReactToastify.css";
