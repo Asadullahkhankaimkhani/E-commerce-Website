75eb34fee0efef6e2c0ad95f0ce94dae65e8680f
Asadullah Khan <41535758+Asadullahkhankaimkhani@users.noreply.github.com>
2021-08-09 05:19:22 +0500

    Firebase Admin

---
+++ server/firebase/index.js
@@ -0,0 +1,7 @@
+var admin = require("firebase-admin");
+
+const serviceAccount = require("../config/firebaseServiceAccountKey.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
