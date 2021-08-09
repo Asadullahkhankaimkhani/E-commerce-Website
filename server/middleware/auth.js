194f89bfe86931a450fd8e9b34489a4b97b4c222
Asadullah Khan <41535758+Asadullahkhankaimkhani@users.noreply.github.com>
2021-08-09 22:31:09 +0500

    Create Or Update User

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,6 +1,17 @@
 const admin = require("../firebase");
 
-exports.authCheck = (req, res, next) => {
-  console.log(req.headers); // token
+exports.authCheck = async (req, res, next) => {
+  //console.log(req.headers); // token
+  try {
+    const firebaseUser = await admin
+      .auth()
+      .verifyIdToken(req.headers.authtoken);
+    // console.log(firebaseUser);
+    req.user = firebaseUser;
+  } catch (err) {
+    res.status(401).json({
+      err: "Invalid Or Expired Token",
+    });
+  }
   next();
 };
