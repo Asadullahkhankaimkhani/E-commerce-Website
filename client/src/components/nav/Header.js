2140439ea34b6325ecc81eca841c2ffe15341bc7
Asadullah Khan <41535758+Asadullahkhankaimkhani@users.noreply.github.com>
2021-08-11 06:07:43 +0500

    Dynamic  Dashboard Links

--- client/src/components/nav/Header.js
+++ client/src/components/nav/Header.js
@@ -44,8 +44,16 @@ const Header = () => {
           icon={<SettingOutlined />}
           title={user.email.split("@")[0]}
         >
-          <Item key="setting:1">Option 1</Item>
-          <Item key="setting:2">Option 2</Item>
+          {user && user.role === "subscriber" && (
+            <Item key="dashboard">
+              <Link to="/user/history">Dashboard</Link>
+            </Item>
+          )}
+          {user && user.role === "admin" && (
+            <Item key="dashboard">
+              <Link to="/admin/dashboard">Dashboard</Link>
+            </Item>
+          )}
           <Item key="log-out" icon={<LogoutOutlined />} onClick={logout}>
             Logout
           </Item>
