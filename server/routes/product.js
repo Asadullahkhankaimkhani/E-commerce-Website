6c505208204144ccf593d6456c573c43d29e9fe1
Asadullah Khan <41535758+Asadullahkhankaimkhani@users.noreply.github.com>
2021-08-14 11:56:24 +0500

    Get Endpoint Product API

--- server/routes/product.js
+++ server/routes/product.js
@@ -2,9 +2,9 @@ const express = require("express");
 
 const router = express.Router();
 // controllers
-const { create } = require("../controllers/product");
+const { create ,read} = require("../controllers/product");
 // middleware
 const { authCheck, adminCheck } = require("../middleware/auth");
 router.post("/product", authCheck, adminCheck, create);
-
+router.get("/products",read);
 module.exports = router;
