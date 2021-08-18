f588c1df674d1d8d833df94afdf5cb0ac35f2ea9
Asadullah Khan <41535758+Asadullahkhankaimkhani@users.noreply.github.com>
2021-08-18 14:50:32 +0500

    Product Delete Endpoint

--- server/controllers/product.js
+++ server/controllers/product.js
@@ -29,3 +29,14 @@ exports.listAll = async (req, res) => {
     .exec();
   res.json(products);
 };
+
+exports.remove = async (req, res) => {
+  try {
+    const deleted = await Product.findOneAndRemove({
+      slug: req.params.slug,
+    }).exec();
+    res.json(deleted);
+  } catch (error) {
+    return res.status(400).send("Product Deleted Failed");
+  }
+};
--- server/routes/product.js
+++ server/routes/product.js
@@ -2,9 +2,10 @@ const express = require("express");
 
 const router = express.Router();
 // controllers
-const { create, listAll } = require("../controllers/product");
+const { create, listAll, remove } = require("../controllers/product");
 // middleware
 const { authCheck, adminCheck } = require("../middleware/auth");
 router.post("/product", authCheck, adminCheck, create);
 router.get("/products/:count", listAll);
+router.delete("/product/:slug", authCheck, remove);
 module.exports = router;
