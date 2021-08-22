e74b4d7287d8aa3398e2e95751def3a06328ea5f
Asadullah Khan <41535758+Asadullahkhankaimkhani@users.noreply.github.com>
2021-08-22 21:42:16 +0500

    Fetch Product to Home Component

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,9 +1,23 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { getProductByCount } from "../functions/product";
 
 const Home = () => {
+  const [produuct, setProduct] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    loadingProduct();
+  }, []);
+
+  const loadingProduct = async () => {
+    const res = await getProductByCount(3);
+    setProduct(res.data);
+  };
+
   return (
     <div>
       <p>React Home</p>
+      {JSON.stringify(produuct)}
     </div>
   );
 };
