6655a9041208882d99474ec8947aa9d1908e9096
Asadullah Khan <41535758+Asadullahkhankaimkhani@users.noreply.github.com>
2021-08-23 18:08:39 +0500

    Using Ant Design Card

--- client/src/components/cards/ProductCard.js
+++ client/src/components/cards/ProductCard.js
@@ -1,7 +1,43 @@
 import React from "react";
+import { Card } from "antd";
+import { EyeOutlined, ShoppingCartOutlined } from "@ant-design/icons";
+import laptop from "../../images/laptop.jpg";
+import { Link } from "react-router-dom";
 
 const ProductCard = ({ product }) => {
-  return <div>{product.title}</div>;
+  const {Meta} = Card;
+
+
+  const { images, title, description, slug } = product;
+
+  return (
+    <Card
+      cover={
+        <img
+          alt=""
+          src={images && images.length ? images[0].url : laptop}
+          style={{ height: "150px", objectFit: "cover" }}
+          className="p-1"
+        />
+      }
+      actions={[
+        <Link to={`/product/${slug}`}>
+          <EyeOutlined className="text-warning" />
+          <br />
+          View Product
+        </Link>,
+        <>
+          <ShoppingCartOutlined className="text-danger" />
+          <br /> add to Card{" "}
+        </>,
+      ]}
+    >
+      <Meta
+        title={title}
+        description={`${description && description.substring(0, 40)}...`}
+      />
+    </Card>
+  );
 };
 
 export default ProductCard;
