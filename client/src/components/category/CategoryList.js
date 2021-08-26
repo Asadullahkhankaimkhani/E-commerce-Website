b7c4923ff9f85400f3c5ea77cf36cdb1463436ed
Asadullah Khan <41535758+Asadullahkhankaimkhani@users.noreply.github.com>
2021-08-26 22:16:42 +0500

    Category Page With Product

--- client/src/components/category/CategoryList.js
+++ client/src/components/category/CategoryList.js
@@ -1,4 +1,4 @@
-import lib, { LoadingOutlined } from "@ant-design/icons";
+import { LoadingOutlined } from "@ant-design/icons";
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { getCategories } from "../../functions/category";
@@ -18,7 +18,7 @@ const CategoryList = () => {
     categories.map((c) => (
       <div
         key={c._id}
-        className="col m-2 btn btn-outlined-primary btn-lg btn-block btn-raised"
+        className="col btn btn-outlined-primary btn-lg btn-block btn-raised m-3"
       >
         <Link to={`/category/${c.slug}`}>{c.name}</Link>
       </div>
@@ -27,12 +27,7 @@ const CategoryList = () => {
     <div className="container ">
       <div className="row ">
         {loading ? (
-          <div>
-            <h3>
-              {" "}
-              <LoadingOutlined />
-            </h3>
-          </div>
+          <h4 className="text-center">Loading...</h4>
         ) : (
           showCategories()
         )}
--- client/src/pages/category/CategoryHome.js
+++ client/src/pages/category/CategoryHome.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import { getCategory } from "../../functions/category";
 import { Link } from "react-router-dom";
 import ProductCard from "../../components/cards/ProductCard";
+import { LoadingOutlined } from "@ant-design/icons";
 
 const CategoryHome = ({ match }) => {
   const [category, setCategory] = useState([]);
@@ -12,10 +13,39 @@ const CategoryHome = ({ match }) => {
 
   useEffect(() => {
     setLoading(true);
-    getCategory(slug).then(({ data }) => setCategory(data));
+    getCategory(slug).then(({ data }) => {
+      setCategory(data.category);
+      setProduct(data.products);
+      setLoading(false);
+    });
   }, []);
 
-  return <div>{JSON.stringify(category)}</div>;
+  return (
+    <div className="container-fluid">
+      <div className="row">
+        <div className="col">
+          {loading ? (
+            <h4 className="text-center p-3 mt-5 mb-5 display-4 jumbotron">
+              <LoadingOutlined />
+            </h4>
+          ) : (
+            <h4 className="text-center p-3 mt-5 mb-5 display-4 jumbotron">
+              {`${product.length}`} Found in this "{`${category.name}`}"
+              Category
+            </h4>
+          )}
+        </div>
+      </div>
+
+      <div className="row">
+        {product.map((p) => (
+          <div key={p._id} className="col-md-3">
+            <ProductCard product={p} />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default CategoryHome;
