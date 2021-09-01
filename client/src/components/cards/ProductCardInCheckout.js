f952f11c6fe6f895d73e198d1502ae9044e77e59
Asadullah Khan <41535758+Asadullahkhankaimkhani@users.noreply.github.com>
2021-09-01 21:53:33 +0500

    React Model Image

--- client/src/components/cards/ProductCardInCheckout.js
+++ client/src/components/cards/ProductCardInCheckout.js
@@ -1,10 +1,18 @@
 import React from "react";
+import laptop from "../../images/laptop.jpg";
+import ModalImage from "react-modal-image";
 
 const ProductCardInCheckout = ({ p }) => {
   return (
     <tbody>
       <tr>
-        <td>image</td>
+        <td style={{ width: "100px", height: "auto" }}>
+          {p.images.length ? (
+            <ModalImage small={p.images[0].url} large={p.images[0].url} />
+          ) : (
+            <ModalImage small={laptop} large={laptop} />
+          )}
+        </td>
         <td>{p.title}</td>
         <td>{p.price}</td>
         <td>{p.brand}</td>
