84f4f8574e0a1513bbac4f9d44760a7dec27364f
Asadullah Khan <41535758+Asadullahkhankaimkhani@users.noreply.github.com>
2021-09-02 03:45:43 +0500

    Pick A Color in Cart Page

--- client/src/components/cards/ProductCardInCheckout.js
+++ client/src/components/cards/ProductCardInCheckout.js
@@ -1,8 +1,30 @@
 import React from "react";
 import laptop from "../../images/laptop.jpg";
 import ModalImage from "react-modal-image";
+import { useDispatch } from "react-redux";
 
 const ProductCardInCheckout = ({ p }) => {
+  const color = ["Black", "Brown", "Silver", "White", "Blue"];
+
+  const dispatch = useDispatch();
+
+  const handleColorChange = (e) => {
+    let cart = [];
+    if (typeof window !== "undefined") {
+      cart = JSON.parse(localStorage.getItem("cart"));
+    }
+    cart.map((product, i) => {
+      if (product._id === p._id) {
+        cart[i].color = e.target.value;
+      }
+    });
+    localStorage.setItem("cart", JSON.stringify(cart));
+    dispatch({
+      type: "ADD_TO_CART",
+      payload: cart,
+    });
+  };
+
   return (
     <tbody>
       <tr>
@@ -16,7 +38,30 @@ const ProductCardInCheckout = ({ p }) => {
         <td>{p.title}</td>
         <td>{p.price}</td>
         <td>{p.brand}</td>
-        <td>{p.color}</td>
+        <td>
+          <select
+            name="color"
+            onChange={handleColorChange}
+            className="form-control"
+          >
+            {p.color ? (
+              <option key={p.color} value={p.color}>
+                {p.color}
+              </option>
+            ) : (
+              <option>
+                <select>Select</select>
+              </option>
+            )}
+            {color
+              .filter((c) => c !== p.color)
+              .map((c) => (
+                <option key={c} value={c}>
+                  {c}
+                </option>
+              ))}
+          </select>
+        </td>
         <td>{p.count}</td>
         <td>Shipping</td>
         <td>Delete Icon</td>
