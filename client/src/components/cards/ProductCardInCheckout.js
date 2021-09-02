c4cb44b9c23c7092d672118b63dfd770f5b0d7ca
Asadullah Khan <41535758+Asadullahkhankaimkhani@users.noreply.github.com>
2021-09-02 18:46:09 +0500

    Quantity Update in Cart

--- client/src/components/cards/ProductCardInCheckout.js
+++ client/src/components/cards/ProductCardInCheckout.js
@@ -2,6 +2,7 @@ import React from "react";
 import laptop from "../../images/laptop.jpg";
 import ModalImage from "react-modal-image";
 import { useDispatch } from "react-redux";
+import { toast } from "react-toastify";
 
 const ProductCardInCheckout = ({ p }) => {
   const color = ["Black", "Brown", "Silver", "White", "Blue"];
@@ -11,13 +12,42 @@ const ProductCardInCheckout = ({ p }) => {
   const handleColorChange = (e) => {
     let cart = [];
     if (typeof window !== "undefined") {
-      cart = JSON.parse(localStorage.getItem("cart"));
+      if (localStorage.getItem("cart")) {
+        cart = JSON.parse(localStorage.getItem("cart"));
+      }
     }
     cart.map((product, i) => {
       if (product._id === p._id) {
         cart[i].color = e.target.value;
       }
     });
+    localStorage.setItem("cart", JSON.stringify(cart));
+    dispatch({
+      type: "ADD_TO_CART",
+      payload: cart,
+    });
+  };
+
+  const handleQunatityChange = (e) => {
+    let count = e.target.value < 1 ? 1 : e.target.value;
+
+    if (count > p.quantity) {
+      toast.error(`Maximium Available Quantity is ${p.quantity}`);
+      return;
+    }
+
+    let cart = [];
+    if (typeof window !== "undefined") {
+      if (localStorage.getItem("cart")) {
+        cart = JSON.parse(localStorage.getItem("cart"));
+      }
+    }
+    cart.map((product, i) => {
+      if (product._id == p._id) {
+        cart[i].count = count;
+      }
+    });
+
     localStorage.setItem("cart", JSON.stringify(cart));
     dispatch({
       type: "ADD_TO_CART",
@@ -62,7 +92,14 @@ const ProductCardInCheckout = ({ p }) => {
               ))}
           </select>
         </td>
-        <td>{p.count}</td>
+        <td className="text-center">
+          <input
+            type="number"
+            className="form-control"
+            value={p.count}
+            onChange={handleQunatityChange}
+          />
+        </td>
         <td>Shipping</td>
         <td>Delete Icon</td>
       </tr>
