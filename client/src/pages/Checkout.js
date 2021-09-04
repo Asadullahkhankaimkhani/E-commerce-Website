4728e45064b9c75679c805a5a35b28be899039bd
Asadullah Khan <41535758+Asadullahkhankaimkhani@users.noreply.github.com>
2021-09-04 14:44:26 +0500

    get user cart in Frontend

--- client/src/pages/Checkout.js
+++ client/src/pages/Checkout.js
@@ -1,6 +1,21 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getUserCart } from "../functions/user";
 
 const Checkout = () => {
+  const [products, setProducts] = useState([]);
+  const [total, setTotal] = useState(0);
+
+  const dispatch = useDispatch();
+  const { user } = useSelector((state) => ({ ...state }));
+
+  useEffect(() => {
+    getUserCart(user.token).then(({ data }) => {
+      setProducts(data.products);
+      setTotal(data.cartTotal);
+    });
+  }, []);
+
   const saveAddresstoDb = () => {
     //
   };
@@ -22,9 +37,9 @@ const Checkout = () => {
       <div className="col-md-6">
         <h4>Order Summary </h4>
         <hr />
-        <p>Products</p>
+        <p>{JSON.stringify(products)}</p>
         <hr />
-        <p>Cart Total: $</p>
+        <p>Cart Total: ${total}</p>
         <div className="row">
           <div className="col-md-6">
             <button className="btn btn-primary">Place Order</button>
