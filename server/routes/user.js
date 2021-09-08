0698b357142e2d6fdf88d3631fd7bdcf606af1da
Asadullah Khan <41535758+Asadullahkhankaimkhani@users.noreply.github.com>
2021-09-08 19:41:29 +0500

    Apply Discount Backend

--- server/routes/user.js
+++ server/routes/user.js
@@ -7,12 +7,17 @@ const {
   getUserCart,
   emptyCart,
   saveAddress,
+  applyCouponToUserCart,
 } = require("../controllers/user");
 
 router.post("/user/cart", authCheck, userCart);
 router.get("/user/cart", authCheck, getUserCart);
 router.delete("/user/cart", authCheck, emptyCart);
 router.post("/user/address", authCheck, saveAddress);
+
+// coupon
+router.post("/user/cart/coupon", authCheck, applyCouponToUserCart);
+
 // router.get("/user", (req, res) => {
 //   res.json({ name: "Asadullah Khan" });
 // });
