6ecfd2af4e213eef857040a067e0d36f7d6e550b
Asadullah Khan <41535758+Asadullahkhankaimkhani@users.noreply.github.com>
2021-09-12 18:40:07 +0500

    Showing Each Order's Products in Table

--- client/src/pages/user/History.js
+++ client/src/pages/user/History.js
@@ -21,9 +21,40 @@ const History = () => {
     console.log(data);
   };
 
-  const showOrderInTable = (order) => {
-    //
-  };
+  const showOrderInTable = (order) => (
+    <table className="table table-bordered">
+      <thead className="thead-light">
+        <tr>
+          <th scope="col">Title</th>
+          <th scope="col">Price</th>
+          <th scope="col">Brands</th>
+          <th scope="col">Color</th>
+          <th scope="col">Count</th>
+          <th scope="col">Shipping</th>
+        </tr>
+      </thead>
+      <tbody>
+        {order.products.map((p, i) => (
+          <tr key={i}>
+            <td>
+              <b>{p.product.title}</b>
+            </td>
+            <td>{p.product.price}</td>
+            <td>{p.product.brand}</td>
+            <td>{p.color}</td>
+            <td>{p.count}</td>
+            <td>
+              {p.product.shipping === "Yes" ? (
+                <CheckCircleOutlined style={{ color: "green" }} />
+              ) : (
+                <CloseCircleOutlined style={{ color: "red" }} />
+              )}
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
 
   const showEachOrders = () =>
     orders.map((order, i) => (
