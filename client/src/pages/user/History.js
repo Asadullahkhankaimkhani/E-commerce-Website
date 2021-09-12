b3433c56c0ec5e0a2c8a48f0e69452070ceb8a30
Asadullah Khan <41535758+Asadullahkhankaimkhani@users.noreply.github.com>
2021-09-12 18:28:54 +0500

    Purchased Order in Card

--- client/src/pages/user/History.js
+++ client/src/pages/user/History.js
@@ -17,15 +17,39 @@ const History = () => {
 
   const loadOrders = async () => {
     const { data } = await getUserOrders(user.token);
+    setOrders(data);
     console.log(data);
   };
+
+  const showOrderInTable = (order) => {
+    //
+  };
+
+  const showEachOrders = () =>
+    orders.map((order, i) => (
+      <div className="m-5 -3 card" key={i}>
+        <p>show payment info</p>
+        {showOrderInTable(order)}
+        <div className="row">
+          <div className="col">
+            <p>PDF download</p>
+          </div>
+        </div>
+      </div>
+    ));
   return (
     <div className="container-fluid">
       <div className="row">
         <div className="col-sm-2">
           <UserNav />
         </div>
-        <div className="col">User Page</div>
+        <div className="col text-center">
+          <h4>
+            {" "}
+            {orders.length > 0 ? "User purchase orders" : "No purchase orders"}
+          </h4>
+          {showEachOrders()}
+        </div>
       </div>
     </div>
   );
