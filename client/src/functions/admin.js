d28d08129cb895096a25662ac7cef83c5707bd22
Asadullah Khan <41535758+Asadullahkhankaimkhani@users.noreply.github.com>
2021-09-20 19:02:37 +0500

    Load All orders in Admin Dashboard

---
+++ client/src/functions/admin.js
@@ -0,0 +1,19 @@
+import axios from "axios";
+
+export const getOrders = async (authToken) =>
+  await axios.get(`${process.env.REACT_APP_API}/admin/orders`, {
+    headers: {
+      authToken,
+    },
+  });
+
+export const changeStatus = async (orderId, orderStatus, authToken) =>
+  await axios.put(
+    `${process.env.REACT_APP_API}/admin`,
+    { orderId, orderStatus },
+    {
+      headers: {
+        authToken,
+      },
+    }
+  );
--- client/src/pages/admin/AdminDashboard.js
+++ client/src/pages/admin/AdminDashboard.js
@@ -1,7 +1,29 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import AdminNav from "../../components/nav/AdminNav";
+import { getOrders, changeStatus } from "../../functions/admin";
+import { useSelector, useDispatch } from "react-redux";
+import { toast } from "react-toastify";
 
 const AdminDashboard = () => {
+  const [orders, setOrders] = useState([]);
+  const { user } = useSelector((state) => ({ ...state }));
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    loadOrders();
+  }, []);
+
+  const loadOrders = async () => {
+    const { data } = await getOrders(user.token);
+    console.log(data);
+  };
+
+  const handleStatusChange = async (orderId, orderStatus) => {
+    const { data } = changeStatus(orderId, orderStatus, user.token);
+    toast.success("Status updated");
+    loadOrders();
+  };
+
   return (
     <>
       <div className="container-fluid">
