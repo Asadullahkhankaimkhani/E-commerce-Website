f3f69e9f7d94d6519a5cf57d1f91e483a2431d1a
Asadullah Khan <41535758+Asadullahkhankaimkhani@users.noreply.github.com>
2021-09-20 20:37:35 +0500

    Products Table in Admin Dashboard

--- client/src/components/order/Orders.js
+++ client/src/components/order/Orders.js
@@ -3,14 +3,51 @@ import { CheckCircleOutlined, CloseCircleOutlined } from "@ant-design/icons";
 import ShowPaymentInfo from "../cards/ShowPaymentInfo";
 
 const Orders = ({ orders, handleStatusChange }) => {
+  const showOrderInTable = (order) => (
+    <table className="table table-bordered">
+      <thead className="thead-light">
+        <tr>
+          <th scope="col">Title</th>
+          <th scope="col">Price</th>
+          <th scope="col">Brand</th>
+          <th scope="col">Color</th>
+          <th scope="col">Count</th>
+          <th scope="col">Shipping</th>
+        </tr>
+      </thead>
+
+      <tbody>
+        {order.products.map((p, i) => (
+          <tr key={i}>
+            <td>
+              <b>{p.product.title}</b>
+            </td>
+            <td>{p.product.price}</td>
+            <td>{p.product.brand}</td>
+            <td>{p.color}</td>
+            <td>{p.count}</td>
+            <td>
+              {p.product.shipping === "Yes" ? (
+                <CheckCircleOutlined style={{ color: "green" }} />
+              ) : (
+                <CloseCircleOutlined style={{ color: "red" }} />
+              )}
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+
   return (
-    <div>
+    <>
       {orders.map((order) => (
         <div key={order._id} className="row pb-5">
           <div className="btn btn-block bg-light">
             <ShowPaymentInfo order={order} showStatus={false} />
+
             <div className="row">
-              <div className="col-md-4">Deivery Status</div>
+              <div className="col-md-4">Delivery Status</div>
               <div className="col-md-8">
                 <select
                   onChange={(e) =>
@@ -29,9 +66,11 @@ const Orders = ({ orders, handleStatusChange }) => {
               </div>
             </div>
           </div>
+
+          {showOrderInTable(order)}
         </div>
       ))}
-    </div>
+    </>
   );
 };
 
